e6d27d79a530ca5c02149006582764b79157eb14
Mathieu Houdet <mathieu.houdet@gmail.com>
2019-03-03 21:16:28 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,6 @@
 var playlist = new Object({Heho: "Song1"});
 
 function updatePlaylist(obj,key,value){
-  return Object.assign({}, obj, {[key]: value});
+  Object.assign({}, obj, {[key]: value});
+  return playlist;
 }
